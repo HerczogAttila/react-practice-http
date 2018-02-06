f334126fb752934abe7af2690d37d5f8ac72440a
Herczog Attila <aherczog@precognox.com>
2018-02-06 10:52:54 +0100

    178. Switching Between Pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,12 +3,13 @@ import {Route} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 177
+      status: 178
     }
   }
 
@@ -27,6 +28,7 @@ class Blog extends Component {
         {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
         {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
