8e4d466153a2d97072e85252e4d4698c30a9f7ec
Herczog Attila <aherczog@precognox.com>
2018-02-06 10:49:14 +0100

    177. Rendering Component for Routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 175
+      status: 177
     }
   }
 
@@ -23,8 +24,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/" render={() => <h1>Home 2</h1>} />
+        {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
+        {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
