9007d88d612e579a795f80ae0e3210d98fd23370
Herczog Attila <aherczog@precognox.com>
2018-02-06 11:54:07 +0100

    186. Extracting Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,7 @@ class Blog extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 185
+      status: 186
     }
   }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
@@ -14,7 +14,7 @@ axios.interceptors.request.use(request => {
   return request;
 }, error => {
   console.log(error);
-  return Promise.reject(error); 
+  return Promise.reject(error);
 });
 
 axios.interceptors.response.use(response => {
@@ -22,7 +22,7 @@ axios.interceptors.response.use(response => {
   return response;
 }, error => {
   console.log(error);
-  return Promise.reject(error); 
+  return Promise.reject(error);
 });
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
